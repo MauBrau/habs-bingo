6efc84503bc2e31585b86c728d34ad8f919fd701
Maude <maude.braunstein@gmail.com>
2026-02-03 01:12:35 -0500

    Cache card

--- habs-bingo-webapp/app/components/BingoCard.tsx
+++ habs-bingo-webapp/app/components/BingoCard.tsx
@@ -9,70 +9,91 @@ import {
     Player,
     PlayerPositions,
 } from "../interface/IBingoBoard";
-import { JSX, ReactElement, useCallback, useEffect, useState } from "react";
-import BingoBoardData from '../assets/bingo-board.json';
+import { useEffect, useState } from "react";
+import BingoBoardData from "../assets/bingo-board.json";
 
 export const thisIsAnUnusedExport = "this export only exists to disable fast refresh for this file";
+
 export default function BingoCard() {
-    const [bingoData, setBingoData] = useState<BingoBoard>(BingoBoardData as BingoBoard);
-    const [bingoOptions, setBingoOptions] = useState<BingoOptions>(bingoData.bingoOptions);
-    const [players, setPlayers] = useState<Player[]>(bingoData.players.filter((player: Player) => player.isActivePlayer && player.position !== PlayerPositions.Goalie));
+    const [bingoData, setBingoData] = useState<BingoBoard>(
+        BingoBoardData as BingoBoard,
+    );
+    const bingoOptions = bingoData.bingoOptions;
+
+    const [players, setPlayers] = useState<Player[]>(
+        bingoData.players.filter(
+            (player: Player) =>
+                player.isActivePlayer &&
+                player.position !== PlayerPositions.Goalie,
+        ),
+    );
     const [tiles, setTiles] = useState<string[]>([]);
 
     let usedPlayersCount = 0, usedPenaltyCount = 0;
 
     const NUM_TILES: number = 25;
+    const FREE_SPACE: number = 12;
 
     useEffect(() => {
-        setTiles(generateBoard());
+        if (localStorage.getItem('tiles') && localStorage.getItem('generationDate')) {
+            const lastDate = new Date(localStorage.getItem('generationDate') as string);
+            const timeSince = new Date().getTime() - lastDate.getTime();
+            if (timeSince >= 60 * 60 * 24 * 1000 ) {
+                storeBoard();
+            } else {
+                const existingBoard = JSON.parse(localStorage.getItem('tiles') as string);
+                setTiles(existingBoard)
+            }
+        } else {
+            storeBoard();
+        }
+        
     }, []);
-   
+
+    function storeBoard() {
+        let generatedBoard = generateBoard();
+        generatedBoard[FREE_SPACE] = "FREE";
+        localStorage.setItem('tiles', JSON.stringify(generatedBoard));
+        localStorage.setItem('generationDate', new Date().toString());
+        setTiles(generatedBoard);
+    }
+
     function generateBoard() {
-        // TODO: Make an array of randomly selected bingo options. No duplicates if theyre generic.
-        // Up to 1 dup if player specific
-        let generatedBoard : string[] = [];
-        console.log('bingo data:   ');
-        console.log(bingoData);
+        let generatedBoard: string[] = [];
         for (let i = 0; i < NUM_TILES; i++) {
             let bingoTileOption: BingoTileOption, randomValue: number;
             do {
                 randomValue = getRandomInt(bingoData.bingoTileOptions.length);
                 bingoTileOption = bingoData.bingoTileOptions[randomValue];
-            } while (conditionChecker(bingoTileOption, bingoOptions));
+            } while (tileTypeChecker(bingoTileOption, bingoOptions));
 
-            bingoTileOption.isOnCardCount = bingoTileOption.isOnCardCount ? bingoTileOption.isOnCardCount + 1 : 1;
+            bingoTileOption.isOnCardCount = (bingoTileOption.isOnCardCount ?? 0) + 1;
 
             if (bingoTileOption.type === BingoCardType.PlayerSpecific) {
                 let randomPlayer: Player;
                 do {
                     randomPlayer = players[getRandomInt(players.length)];
-                } while ( randomPlayer.isOnCard );
+                } while (randomPlayer.isOnCard);
 
                 randomPlayer.isOnCard = true;
                 usedPlayersCount++;
-                generatedBoard.push(`${randomPlayer.name}${bingoTileOption.text}`);
-
+                generatedBoard.push(
+                    `${randomPlayer.name}${bingoTileOption.text}`,
+                );
             } else if (bingoTileOption.type === BingoCardType.Penalty) {
-                let penaltyOption : PenaltyTypes;
+                let penaltyOption: PenaltyTypes;
                 do {
                     penaltyOption =
                         bingoData.penaltyTypes[
                             getRandomInt(bingoData.penaltyTypes.length)
                         ];
                 } while (penaltyOption.isOnCard);
+
                 penaltyOption.isOnCard = true;
                 usedPenaltyCount++;
                 generatedBoard.push("Penalty:" + penaltyOption.text);
-            }
-            else {
+            } else {
                 generatedBoard.push(bingoTileOption.text);
-            }
-
-            // If we run out of players for player-specific tile options (somehow), prevent the code from pulling player-specific options
-            if (usedPlayersCount === players.length) {
-                bingoData.bingoTileOptions = bingoData.bingoTileOptions.filter(
-                    (option : BingoTileOption) => option.type !== BingoCardType.PlayerSpecific,
-                );
             }
         }
         return generatedBoard;
@@ -82,14 +103,17 @@ export default function BingoCard() {
         return Math.floor(Math.random() * max);
     }
 
-    function conditionChecker(tileType: BingoTileOption, bingoOptions: BingoOptions) {
+    function tileTypeChecker(
+        tileType: BingoTileOption,
+        bingoOptions: BingoOptions,
+    ) {
         let type = tileType.type;
 
         // TODO: Clean up, it's ugly
-        if (type === BingoCardType.Penalty && usedPenaltyCount === bingoOptions.penaltyLimit) {
+        if ( type === BingoCardType.Penalty && usedPenaltyCount === bingoOptions.penaltyLimit ) {
             return true;
         }
-        if (type === BingoCardType.PlayerSpecific && usedPlayersCount === bingoOptions.playerLimit) {
+        if ( type === BingoCardType.PlayerSpecific && usedPlayersCount === bingoOptions.playerLimit ) {
             return true;
         }
         if (tileType.isOnCardCount) {
@@ -102,12 +126,15 @@ export default function BingoCard() {
         return false;
     }
 
-    return (
-        tiles.length > 0 ?
+    return tiles.length > 0 ? (
         <div className="items-center bg-white max-w-s p-6 rounded-lg">
             <div className={`h-200 w-200 grid grid-cols-5 grid-rows-5 gap-4`}>
-                {tiles.map((tile, index) => <BingoTile key={index} text={tile} />)}
+                {tiles.map((tile, index) => (
+                    <BingoTile key={index} text={tile} />
+                ))}
             </div>
-        </div> : <div/>
+        </div>
+    ) : (
+        <div />
     );
 }
