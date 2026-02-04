0190dd9933c69a1844d2c742a495c287cd1222de
Maude <maude.braunstein@gmail.com>
2026-02-04 17:22:23 -0500

    Card refresh and some style changes
    Currently buggy but want to do a test run tonight

--- habs-bingo-webapp/app/components/BingoCard.tsx
+++ habs-bingo-webapp/app/components/BingoCard.tsx
@@ -10,10 +10,13 @@ import {
     PlayerPositions,
     TileStatus,
 } from "../interface/IBingoBoard";
+import Image from "next/image";
+import { Dialog, DialogTitle } from "@mui/material";
 import { useEffect, useState } from "react";
 import BingoBoardData from "../assets/bingo-board.json";
 
-export const thisIsAnUnusedExport = "this export only exists to disable fast refresh for this file";
+export const thisIsAnUnusedExport =
+    "this export only exists to disable fast refresh for this file";
 
 export default function BingoCard() {
     const [bingoData, setBingoData] = useState<BingoBoard>(
@@ -29,40 +32,48 @@ export default function BingoCard() {
         ),
     );
     const [tiles, setTiles] = useState<TileStatus[]>([]);
+    const [open, setOpen] = useState<boolean>(false);
 
-    let usedPlayersCount = 0, usedPenaltyCount = 0;
+    let usedPlayersCount = 0,
+        usedPenaltyCount = 0;
 
     const NUM_TILES: number = 25;
     const FREE_SPACE: number = 12;
 
     useEffect(() => {
-        if (localStorage.getItem('tiles') && localStorage.getItem('generationDate')) {
-            const lastDate = new Date(localStorage.getItem('generationDate') as string);
+        if (
+            localStorage.getItem("tiles") &&
+            localStorage.getItem("generationDate")
+        ) {
+            const lastDate = new Date(
+                localStorage.getItem("generationDate") as string,
+            );
             const timeSince = new Date().getTime() - lastDate.getTime();
-            if (timeSince >= 60 * 60 * 24 * 1000 ) {
+            if (timeSince >= 60 * 60 * 24 * 1000) {
                 storeBoard();
             } else {
-                const existingBoard = JSON.parse(localStorage.getItem('tiles') as string);
-                setTiles(existingBoard)
+                const existingBoard = JSON.parse(
+                    localStorage.getItem("tiles") as string,
+                );
+                setTiles(existingBoard);
             }
         } else {
             storeBoard();
         }
-        
     }, []);
 
-    function storeBoard() {
+    const storeBoard = () => {
         let generatedBoard = generateBoard();
-        generatedBoard[FREE_SPACE] =  {
+        generatedBoard[FREE_SPACE] = {
             text: "FREE",
-            isChecked: false
+            isChecked: true,
         };
-        localStorage.setItem('tiles', JSON.stringify(generatedBoard));
-        localStorage.setItem('generationDate', new Date().toString());
+        localStorage.setItem("tiles", JSON.stringify(generatedBoard));
+        localStorage.setItem("generationDate", new Date().toString());
         setTiles(generatedBoard);
-    }
+    };
 
-    function generateBoard() {
+    const generateBoard = () => {
         let generatedBoard: TileStatus[] = [];
         for (let i = 0; i < NUM_TILES; i++) {
             let bingoTileOption: BingoTileOption, randomValue: number;
@@ -71,7 +82,8 @@ export default function BingoCard() {
                 bingoTileOption = bingoData.bingoTileOptions[randomValue];
             } while (tileTypeChecker(bingoTileOption, bingoOptions));
 
-            bingoTileOption.isOnCardCount = (bingoTileOption.isOnCardCount ?? 0) + 1;
+            bingoTileOption.isOnCardCount =
+                (bingoTileOption.isOnCardCount ?? 0) + 1;
 
             if (bingoTileOption.type === BingoCardType.PlayerSpecific) {
                 let randomPlayer: Player;
@@ -81,12 +93,10 @@ export default function BingoCard() {
 
                 randomPlayer.isOnCard = true;
                 usedPlayersCount++;
-                generatedBoard.push(
-                    {
-                        text: `${randomPlayer.name}${bingoTileOption.text}`,
-                        isChecked: false
-                    }
-                );
+                generatedBoard.push({
+                    text: `${randomPlayer.name}${bingoTileOption.text}`,
+                    isChecked: false,
+                });
             } else if (bingoTileOption.type === BingoCardType.Penalty) {
                 let penaltyOption: PenaltyTypes;
                 do {
@@ -98,39 +108,41 @@ export default function BingoCard() {
 
                 penaltyOption.isOnCard = true;
                 usedPenaltyCount++;
-                generatedBoard.push(
-                    {
-                        text: "Penalty:" + penaltyOption.text,
-                        isChecked: false
-                    }
-                );
+                generatedBoard.push({
+                    text: "Penalty:" + penaltyOption.text,
+                    isChecked: false,
+                });
             } else {
-                generatedBoard.push(
-                    {
-                        text: bingoTileOption.text,
-                        isChecked: false
-                    }
-                );
+                generatedBoard.push({
+                    text: bingoTileOption.text,
+                    isChecked: false,
+                });
             }
         }
         return generatedBoard;
-    }
+    };
 
-    function getRandomInt(max: number) {
+    const getRandomInt = (max: number) => {
         return Math.floor(Math.random() * max);
-    }
+    };
 
-    function tileTypeChecker(
+    const tileTypeChecker = (
         tileType: BingoTileOption,
         bingoOptions: BingoOptions,
-    ) {
+    ) => {
         let type = tileType.type;
 
         // TODO: Clean up, it's ugly
-        if ( type === BingoCardType.Penalty && usedPenaltyCount === bingoOptions.penaltyLimit ) {
+        if (
+            type === BingoCardType.Penalty &&
+            usedPenaltyCount === bingoOptions.penaltyLimit
+        ) {
             return true;
         }
-        if ( type === BingoCardType.PlayerSpecific && usedPlayersCount === bingoOptions.playerLimit ) {
+        if (
+            type === BingoCardType.PlayerSpecific &&
+            usedPlayersCount === bingoOptions.playerLimit
+        ) {
             return true;
         }
         if (tileType.isOnCardCount) {
@@ -141,18 +153,106 @@ export default function BingoCard() {
             }
         }
         return false;
-    }
+    };
 
-    const handleClick = (tile: TileStatus, index: number) => {
+    const handleClick = (index: number) => {
         tiles[index].isChecked = !tiles[index].isChecked;
-        localStorage.setItem('tiles', JSON.stringify(tiles));
+        localStorage.setItem("tiles", JSON.stringify(tiles));
+    };
+
+    const clearOptions = () => {
+        // reset board data
+        // setBingoData({...BingoBoardData} as BingoBoard); // i Wish you worked
+        let newBingoData = { ...bingoData };
+        newBingoData.bingoTileOptions = newBingoData.bingoTileOptions.map(
+            (bingoTileOption: BingoTileOption) => {
+                bingoTileOption.isOnCardCount = 0;
+                return bingoTileOption;
+            },
+        );
+
+        newBingoData.penaltyTypes = newBingoData.penaltyTypes.map(
+            (penalty: PenaltyTypes) => {
+                penalty.isOnCard = false;
+                return penalty;
+            },
+        );
+
+        let newPlayers = players.map((player: Player) => {
+            player.isOnCard = false;
+            return player;
+        });
+
+        setPlayers(newPlayers);
+        setBingoData(newBingoData);
+        setTiles([]);
+
+        usedPenaltyCount = 0;
+        usedPlayersCount = 0;
+    };
+
+    const newCardConfirmation = () => {
+        setOpen(true);
+    };
+    const handleConfirm = () => {
+        clearOptions();
+        storeBoard();
+        setOpen(false);
+    };
+    const handleClose = () => {
+        setOpen(false);
     };
 
     return tiles.length > 0 ? (
-        <div className="items-center bg-white max-w-s p-6 rounded-lg">
-            <div className={`h-200 w-200 grid grid-cols-5 grid-rows-5 gap-4`}>
+        <div className="bg-white max-w-s p-6 rounded-lg">
+            <div className="pb-4">
+                <div className="flex items-center justify-between">
+                    <Image
+                        src="/unofficial-habs-bingo.svg"
+                        alt="Unofficial Habs Bingo logo"
+                        width={525}
+                        height={150}
+                        priority
+                    />
+                    <button
+                        onClick={newCardConfirmation}
+                        className="bg-habs-red hover:bg-habs-blue text-white font-bold py-2 px-4 rounded cursor-pointer"
+                    >
+                        New Card?
+                    </button>
+                </div>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        Generate a new card?
+                    </DialogTitle>
+                    <div className="flex items-center justify-between p-6">
+                        <button
+                            className="bg-habs-red hover:bg-habs-blue text-white font-bold py-2 px-4 rounded cursor-pointer"
+                            onClick={handleConfirm}
+                        >
+                            Sure
+                        </button>
+                        <button
+                            className="bg-habs-red hover:bg-habs-blue text-white font-bold py-2 px-4 rounded cursor-pointer"
+                            onClick={handleClose}
+                        >
+                            Never mind
+                        </button>
+                    </div>
+                </Dialog>
+            </div>
+            <div className={`h-200 w-200 grid grid-cols-5 grid-rows-5 gap-3`}>
                 {tiles.map((tile, index) => (
-                    <BingoTile key={index} text={tile.text} isChecked={tile.isChecked} onClick={() => handleClick(tile, index)} />
+                    <BingoTile
+                        key={index}
+                        text={tile.text}
+                        isChecked={tile.isChecked}
+                        onClick={() => handleClick(index)}
+                    />
                 ))}
             </div>
         </div>
--- habs-bingo-webapp/app/page.tsx
+++ habs-bingo-webapp/app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
 import BingoCard from "./components/BingoCard";
 
 export default function Home() {
   return (
     <div>
       <main>
-        <Image
-          className="object-center"
-          src="/unofficial-habs-bingo.svg"
-          alt="Unofficial Habs Bingo logo"
-          width={525}
-          height={150}
-          priority
-        />
         <div className="font-sans items-center justify-items-center min-h-screen p-8">
           <BingoCard />
         </div>
